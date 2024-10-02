76240d464f007a799f30b21f28e016f0b0c9c9f0
Gustavo Charamba <gucharamba@gmail.com>
2024-10-01 22:12:20 -0300

    crypto page

--- frontend/src/components/Crypto/main.js
+++ frontend/src/components/Crypto/main.js
@@ -1,29 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 
-import { MainContainer, HeaderContainer, BodyContainer } from "./mainStyled"
+
+import { MainContainer, HeaderContainer, BodyContainer, CryptoTableWrapper, 
+     TableRow, TableCell, HeaderRow, HeaderCell, CryptoTable } from "./mainStyled"
 
 const Main = () => {
+
+    // const [ cryptos, setCryptos ] = useState([])
+    // const [ loading, setLoading ] = useState(true)
+    // const [error, setError] = useState(null);
+
+    // useEffect(() => {
+    //     const getCryptos = async () => {
+    //         const options = {
+    //             method: 'GET',
+    //             url: 'https://real-time-finance-data.p.rapidapi.com/market-trends',
+    //             params: {
+    //                 trend_type: 'CRYPTO',
+    //                 country: 'us',
+    //                 language: 'en'
+    //             },
+    //             headers: {
+    //                 'x-rapidapi-key': 'e2a99c8790msh90aed4d017ed0b2p129056jsna141f90f1eda',
+    //                 'x-rapidapi-host': 'real-time-finance-data.p.rapidapi.com'
+    //             }
+    //         };
+            
+    //         try {
+    //             const response = await axios.request(options);
+    //             const trends = response.data.data.trends; // Accessing the trends data
+    //             setCryptos(trends); // Updating the state with the data
+    //         } catch (error) {
+    //             setError('Failed to fetch data'); // Setting error state
+    //         } finally {
+    //             setLoading(false); // Marking loading as finished
+    //         }
+    //     };
+
+    //     getCryptos(); // Call the function to fetch data when the component mounts
+    // }, []); // Empty dependency array to run the effect once on mount
+    // console.log(cryptos)
+    // if (loading) return <p>Loading...</p>;
+    // if (error) return <p>{error}</p>;
+
     return(
         <MainContainer>
             <HeaderContainer>
             <div>
-                    <h2>Cryh4to</h2>
+                    <h2>Crypto</h2>
                 </div>
                 <div>
                     <a>Account</a>
                 </div>
             </HeaderContainer>
             <BodyContainer>
-                <div>
-                    <h4>Name</h4>
-                    <h4>h4rice</h4>
-                    <h4>1h % </h4>
-                    <h4>24 %</h4>
-                    <h4>7d %</h4>
-                    <h4>Market Cah4</h4>
-                    <h4>Volume (24h)</h4>
-                </div>
+                {/* <CryptoTableWrapper>
+                    <TableHeader>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Price</TableCell>
+                            <TableCell>1h %</TableCell>
+                            <TableCell>24h %</TableCell>
+                            <TableCell>7d %</TableCell>
+                            <TableCell>Market Cap</TableCell>
+                            <TableCell>Volume (24h)</TableCell>
+                        </TableRow>
+                    </TableHeader>
 
+                    {cryptos.map((crypto, index) => (
+                        <TableRow key={index}>
+                            <TableCell>{crypto.name}</TableCell>
+                            <TableCell>${crypto.price}</TableCell>
+                            <TableCell>{crypto.change_1h}%</TableCell>
+                            <TableCell>{crypto.change_24h}%</TableCell>
+                            <TableCell>{crypto.change_7d}%</TableCell>
+                            <TableCell>${crypto.market_cap}</TableCell>
+                            <TableCell>${crypto.volume_24h}</TableCell>
+                        </TableRow>
+                    ))}
+                    </CryptoTableWrapper> */}
+
+                    <CryptoTableWrapper>
+                        <HeaderRow>
+                            <HeaderCell>Name</HeaderCell>
+                            <HeaderCell>Price</HeaderCell>
+                            <HeaderCell>1h %</HeaderCell>
+                            <HeaderCell>24h %</HeaderCell>
+                            <HeaderCell>7d %</HeaderCell>
+                            <HeaderCell>Market Cap</HeaderCell>
+                            <HeaderCell>Volume (24h)</HeaderCell>
+                        </HeaderRow>
+                        <CryptoTable>
+                            {Array.from({length: 80}).map((_, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>Bitcoin</TableCell>
+                                    <TableCell>$70000</TableCell>
+                                    <TableCell>0.17%</TableCell>
+                                    <TableCell>0.5%</TableCell>
+                                    <TableCell>12%</TableCell>
+                                    <TableCell>$1.000.000.000.000</TableCell>
+                                    <TableCell>$1.000.000.000.000</TableCell>
+                            </TableRow>
+                            ))}
+                        </CryptoTable>
+                    </CryptoTableWrapper>
             </BodyContainer>
         </MainContainer>
     )
--- frontend/src/components/Crypto/mainStyled.js
+++ frontend/src/components/Crypto/mainStyled.js
@@ -1,31 +1,120 @@
 import styled from "styled-components";
-import {colors} from "../../global/globalColors"
+import { colors } from "../../global/globalColors";
 
 export const MainContainer = styled.div`
-    height: 100%;
-    width: 100%;
-    border-radius: 24px;
-    color: whitesmoke;
-    overflow: hidden;
-`
+  height: 100%;
+  width: 100%;
+  border-radius: 24px;
+  color: whitesmoke;
+  overflow: hidden;
+  background-color: ${colors.raisinBlack}; // Assuming this is your background color
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
 
 export const HeaderContainer = styled.div`
-    height: 10%;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 0.5px solid ${colors.onyx};
+  height: 10%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid ${colors.onyx};
+  padding: 0 20px;
 
-    div {
-        width: 250px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+  div {
+    width: 250px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h2 {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  a {
+    color: ${colors.blueJeans};
+    text-decoration: none;
+    font-weight: bold;
+    transition: color 0.3s ease;
+
+    &:hover {
+      color: ${colors.carolinaBlue};
     }
-`
+  }
+`;
 
 export const BodyContainer = styled.div`
-    height: 90%;
-    width: 100%;
+  height: 90%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CryptoTableWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100% - 40px);
+  max-height: calc(100% - 40px);
+  border: 1px solid ${colors.onyx};
+  border-radius: 12px;
+`;
+
+export const HeaderRow = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 2fr 2fr;
+  background-color: ${colors.outerSpace};
+  color: white;
+  font-weight: bold;
+  border-top-right-radius: 12px;
+  border-top-left-radius: 12px;
+`;
+
+export const HeaderCell = styled.div`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
+
+export const CryptoTable = styled.div`
+  overflow-y: auto;
+  
+    &::-webkit-scrollbar {
+    width: 8px;
+  }
+  
+  &::-webkit-scrollbar-track {
+    background: ${colors.raisinBlack};
+  }
+  
+  &::-webkit-scrollbar-thumb {
+    background-color: ${colors.onyx};
+    border-radius: 4px;
+  }
+`
+
+export const TableRow = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 2fr 2fr;
+  color: white;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+  border-bottom: 1px solid ${colors.onyx};
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+export const TableCell = styled.div`
+    height: 60px;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
